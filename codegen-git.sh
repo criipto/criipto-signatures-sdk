c8e6fbe5534e10aca8ffe95491ee62dbd86e2287
Mick Hansen <maker@mhansen.io>
2025-08-08 10:23:28 +0200

    add helper script for codegen
    the script:
    1. creates a branch
    2. runs codegen
    3. makes a commmit
    4. pushes
    5. creates a pull request

---
+++ codegen-git.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+set -euxo pipefail
+date=$(date '+%Y-%m-%d')
+
+test -z "$(git status --porcelain)"
+if [ 0 -ne $? ]; then
+  echo "your working git has ch-ch-chaaaanges, exiting"
+  exit 1
+fi
+
+git checkout -B codegen-$date
+npm run codegen
+
+git add .
+
+git commit -m "codegen: $date"
+
+git push --force origin codegen-$date
+
+gh pr create --title "Codegen: $date" --body "Codegen: $date"
